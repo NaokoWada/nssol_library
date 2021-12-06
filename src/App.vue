a6520260d9ced1139f48c1ddc0c5bf07714d36cf
github NaokoWada <wada.naoko.7yq@jp.nssol.nipponsteel.com>
2021-12-06 16:26:41 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="img">
+      <img alt="Vue logo" src="./assets/img.png">
+    </div>
+      <div id="bookTitle">
+        <p class="title">ベンダーマネジメントの極意</p>
+      </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -24,5 +28,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#img {
+  
 }
 </style>
