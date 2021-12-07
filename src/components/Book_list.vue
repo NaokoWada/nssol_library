e3b5b0073ed522b1a5344573b940211a9e32e311
yoshiki matsui <matsui.yoshiki.ks5@jp.nssol.nipponsteel.com>
2021-12-07 11:05:02 +0900

    WebContentからsrcに移行

--- src/components/Book_list.vue
+++ src/components/Book_list.vue
@@ -1,22 +1,9 @@
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/common.css">
-    <link rel="stylesheet" href="css/main.css">
-    <title>Main</title>
-
-  </head>
-
-  <body>
-
-  </body>
-  
-  <table border="1" style="border-collapse: collapse;">
+<template>
+  <div>
+    <table border="1" style="border-collapse: collapse;">
     <thead style="display: block;">
     <tr>
-    <tytle style="width:300px;"><font size="20";><img src="image/onepiece01_luffy.png">本の一覧<img src="image/onepiece01_luffy.png"></tytle>
+    <tytle style="width:300px;"><img src="../assets/onepiece01_luffy.png">本の一覧<img src="../assets/onepiece01_luffy.png"></tytle>
     </tr>
     </thead>
     <thead style="display: block;">
@@ -36,31 +23,37 @@
     <tbody style="display: block;overflow-y:scroll;height:600px;">
     <tr>
     
-    <td style="width:150px;"><img src="image/book_open_yoko.png"></td>
+    <td style="width:150px;"><img src="../assets/book_open_yoko.png"></td>
     
     <td style="width:150px;">ベンダーマネージメントの極意</td>
     <td style="width:150px;">★★★★★</td>
     <td style="width:150px;">★★★★☆</td>
-    <td style="width:150px;"><img src="image/Specialist.png"></td>
+    <td style="width:150px;"><img src="../assets/Specialist.png"></td>
     <td style="width:150px;">ためになりました！</td>
-    <td style="width:150px;"><img src="image/animal_angora_usagi.png"></td>
+    <td style="width:150px;"><img src="../assets/animal_angora_usagi.png"></td>
     <td style="width:150px;">そんなに読む価値なし！</td>
     </tr>
     <tr>
-      <td style="width:150px;"><img src="image/book_open_yoko.png"></td>
+      <td style="width:150px;"><img src="../assets/book_open_yoko.png"></td>
     
     <td style="width:150px;">データモデル大全</td>
     <td style="width:150px;">★★★★☆</td>
     <td style="width:150px;">★★★☆☆</td>
-    <td style="width:150px;"><img src="image/mask_woman5_faceshield.png"></td>
+    <td style="width:150px;"><img src="../assets/mask_woman5_faceshield.png"></td>
     <td style="width:150px;">データモデリングの実務上の具体例がわかりやすい！</td>
-    <td style="width:150px;"><img src="image/mask_woman6_medical.png"></td>
+    <td style="width:150px;"><img src="../assets/mask_woman6_medical.png"></td>
     <td style="width:150px;">もう少し具体例の難易度を高くすべき</td>
     </tr>
     
     </tbody>
     </table>
+    
+  </div>
 
+</template>
 
-
-</html>
+<script>
+export default {
+  
+}
+</script>
