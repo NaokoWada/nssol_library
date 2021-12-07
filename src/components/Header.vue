628f613bdda919b6786ea7e2167ddbf417867c83
Yuhei Shibata <shibata.yuhei.a42@jp.nssol.nipponsteel.com>
2021-12-07 13:25:11 +0900

    ヘッダーとvue-routerを追加

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav>
+            <router-link to="">Homeへ移動</router-link>
+            <router-link to="">一覧ページへ移動</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
