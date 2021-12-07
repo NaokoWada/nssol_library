7ac7e1a96f09b14a98f600955e99fe0b40635df3
github NaokoWada <wada.naoko.7yq@jp.nssol.nipponsteel.com>
2021-12-07 13:48:56 +0900

    Bookdetail.vueを追加しました

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,15 @@
 <template>
   <div id="app">
-    <div id="book">
-      <div id="img">
-        <img alt="book img" src="./assets/img.png"/>
-      </div>
-      <div id="bookTitle">
-        <p class="title">ベンダーマネジメントの極意</p>
-        <p class="author">著者： 長尾 清一</p>
-        <p class="publisher">出版社： 日経BP</p>
-        <p class="publishYear">出版年： 2009年7月23日</p>
-        <p class="summary">
-          概要：プロジェクトの成功とスキルアップのために、
-          日々現場で奮闘するプロジェクトマネジャーの皆さんにぜひ手にしていただきたい一冊です。
-        </p>
-        <p class="outline">目次</p>
-      </div>
-    </div>
-    <div id="tab">
-    <ul class="tabMenu">
-        <li @click="isSelect('1')">感想</li>
-        <li @click="isSelect('2')">Q＆A</li>
-    </ul>
-    <div class="tabContents">
-        <div v-if="isActive === '1'">
-          <textarea name="" id="" cols="30" rows="10" placeholder="感想を入力してください"></textarea>
-          <button>投稿する</button>
-        </div>
-        <div v-else-if="isActive === '2'">
-          <textarea name="" id="" cols="30" rows="10" placeholder="Q＆Aを入力してください"></textarea>
-          <button>投稿する</button>
-        </div>
-    </div>
-</div>
- 
+    <Bookdetail></Bookdetail>
   </div>
- 
 </template>
 <script>
+import Bookdetail from "./components/Bookdetail.vue"
 export default {
   name: "App",
-  data () {
-    return {
-      isActive: "1"
-    }
+  components: {
+    Bookdetail
   },
-  components: {},
-  methods: {
-        isSelect: function (num) {
-            this.isActive = num;
-        }
-    }
-    
- 
 };
  
 </script>
@@ -65,62 +23,5 @@ export default {
   color: #2c3e50;
     margin-right: 0;
  
-}
-#img {
-  text-align: left;
-  /* padding-right: 50px; */
-  
-}
-.title {
-  font-size: 40px;
-  margin-left: 0;
-  margin-top: 20px;
-  margin-bottom: 10px;
-}
-#book {
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
-}
-#bookTitle {
-  /* padding-left: 50px; */
-  margin-left: 20px;
-  padding-left: 20px;
- 
-  border: solid 1px black;
-  border-width: 100%;
-  margin-right: 0;
-}
- 
-#tab {
-  
-  width: 100%;
-  max-width: 500px;
-  margin: 40px auto;
-}
-.tabMenu {
-    display: flex;
-}   
-    li {
-      width: auto;
-      padding: 10px 20px;
-      color: white;
-      border-right: 1px solid white;
-      background-color: green;
-      cursor: pointer;
-            
-      
-      }
-    
- 
-  
- 
-  .tabContents {
-    width: 100%;
-    padding: 20px;
-    border: 1px solid green;
-    
- 
-  
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+// import Header from './components/Header.vue';
+import Bookdetail from './components/Bookdetail.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-    {},
-    {},
-    {}
+    {path: '/', component: Bookdetail}
 ]
 
 const router = new VueRouter({
