849c91bf3874135602a9e54d2595bb79ada4ffb1
yoshiki matsui <matsui.yoshiki.ks5@jp.nssol.nipponsteel.com>
2021-12-08 15:54:52 +0900

    JSON情報追加。本の数増加

--- src/components/Book_list.vue
+++ src/components/Book_list.vue
@@ -2,7 +2,7 @@
   <div id="app">
     
     <div class="mainbody">
-     <tytle><font size="7" color="black" background-color="red">本の一覧</font><font size="4" color="black">-現在登録されている本は ２ 冊です-</font></tytle>
+     <tytle><font size="7" color="black" background-color="red">本の一覧</font><font size="4" color="black">-現在登録されている本は 6 冊です-</font></tytle>
     </div>
     
     <table border="1" style="border-collapse: collapse;">
@@ -18,6 +18,7 @@
     <th style="width:150px;">タイトル</th>
     <th style="width:150px;">社内評価</th>
     <th style="width:150px;">BU評価</th>
+    <th style="width:150px;">キーワード</th>
     <th style="width:150px;">社員A</th>
     <th style="width:150px;">感想</th>
     <th style="width:150px;">社員B</th>
@@ -32,6 +33,7 @@
     <td style="width:150px;">{{book.title}}</td>
     <td style="width:150px;">{{book.internal}}</td>
     <td style="width:150px;">{{book.bu}}</td>
+    <td style="width:150px;">{{book.keyword}}</td>
     <td style="width:150px;"><img :src="book.employee1"></td>
     <td style="width:150px;">{{book.comment1}}</td>
     <td style="width:150px;"><img :src="book.employee2"></td>
@@ -60,7 +62,29 @@ export default {
              employee1:require('../assets/mask_woman5_faceshield.png'),
              comment1:'データモデリングの実務上の具体例がわかりやすい！',
              employee2:require('../assets/mask_woman6_medical.png'),
-             comment2:'もう少し具体例の難易度を高くすべき'},
+             comment2:'もう少し具体例の難易度を高くすべき',
+             keyword:'データモデル',
+             level:'easy'},
+            {image:require('../assets/NWhard.png'),
+             title:'ネットワーク', 
+             internal:'★★★★☆',
+             bu:'★★★☆☆',
+             employee1:require('../assets/mask_woman5_faceshield.png'),
+             comment1:'前提知識がある程度必要だと感じた。いい本',
+             employee2:require('../assets/mask_woman6_medical.png'),
+             comment2:'最新の高度なネットワーク技術について知ることができた',
+             keyword:'ネットワーク',
+             level:'hard'},
+             {image:require('../assets/NWeasy.png'),
+             title:'ネットワーク構築＆運用がしっかりわかる教科書', 
+             internal:'★★★★★',
+             bu:'★★★★★',
+             employee1:require('../assets/mask_woman5_faceshield.png'),
+             comment1:'図が豊富に使われており分かりやすかった',
+             employee2:require('../assets/mask_woman6_medical.png'),
+             comment2:'初心者でもわかりやすいように説明だった',
+             keyword:'ネットワーク構築',
+             level:'easy'},
             {
              image:require('../assets/vendar.png'),
              title:'ベンダーマネージメントの極意',
@@ -69,7 +93,31 @@ export default {
              employee1:require('../assets/mask_woman5_faceshield.png'),
              comment1:'ためになりました！',
              employee2:require('../assets/mask_woman6_medical.png'),
-             comment2:'そんなに読む価値なし！'}
+             comment2:'そんなに読む価値なし！',
+             keyword:'マネジメント',
+             level:'middle'},
+             {
+             image:require('../assets/dx.jpg'),
+             title:'DXの思考法',
+             internal:'★★☆☆☆',
+             bu:'★★★☆☆',
+             employee1:require('../assets/mask_woman5_faceshield.png'),
+             comment1:'上級者向け',
+             employee2:require('../assets/mask_woman6_medical.png'),
+             comment2:'知りたいことが書いてなかった',
+             keyword:'DX',
+             level:'hard'},
+             {
+             image:require('../assets/NWospf.png'),
+             title:'OSPF Complete Implementation',
+             internal:'★★★★★',
+             bu:'★★★★★',
+             employee1:require('../assets/mask_woman5_faceshield.png'),
+             comment1:'OSPFについて詳しく知りたい人にはとてもオススメの本です！',
+             employee2:require('../assets/mask_woman6_medical.png'),
+             comment2:'OSPFについて完璧に理解できる。ただし内容は英語',
+             keyword:'OSPF',
+             level:'hard'}
              ]
           
     }
