5a9b4d3175dbe826f17f270b8cdc7011b261ecb8
Yuhei Shibata <shibata.yuhei.a42@jp.nssol.nipponsteel.com>
2021-12-09 14:07:03 +0900

    評価を投稿できるように修正しました

--- src/components/BookReview.vue
+++ src/components/BookReview.vue
@@ -1,33 +1,37 @@
 <template>
-    <div id="">
-      <div class="">
-          <textarea
-            name=""
-            id=""
-            cols="50"
-            rows="10"
-            placeholder="感想を入力してください"
-            v-model="inputReview"
-          ></textarea>
-          <button @click="addReview">感想を投稿する</button>
-          <div class="reviews">
-              <ul v-for="review of reviews" :key="review.id">
-                  <li>
-                      <div class="balloon5">
-                          <div class="faceicon">
-                              <img src="../assets/mask_woman5_faceshield.png" alt="">
-                          </div>
-                          <div class="chatting">
-                              <div class="says">
-                                  <p>{{review}}</p>
-                              </div>
-                          </div>
-                      </div>
-                  </li>
-              </ul>
-          </div>
-        </div>
+  <div id="">
+    <div class="">
+      <textarea
+        name=""
+        id=""
+        cols="50"
+        rows="10"
+        placeholder="感想を入力してください"
+        v-model="inputReview"
+      ></textarea>
+      <div id="app">
+        <star-rating @rating-selected ="setRating" v-model="rating" :increment="0.5" :max-rating="5" inactive-color="#000" active-color="#ff0"></star-rating>
+      </div>
+      <button @click="addReview">感想を投稿する</button>
+      <div class="reviews">
+        <ul v-for="review of reviews" :key="review.id">
+          <li>
+            <div class="balloon5">
+              <div class="faceicon">
+                <img src="../assets/mask_woman5_faceshield.png" alt="" />
+              </div>
+              <div class="chatting">
+                <div class="says">
+                  <p>感想：{{ review.review }}</p>
+                  <p>評価：{{review.rating}}</p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -38,13 +42,24 @@ export default {
       isActive: "1",
       inputReview: "",
       reviews: [],
+      rating: 0
     };
   },
   components: {},
   methods: {
     addReview() {
-        this.reviews.unshift (this.inputReview)
-        this.inputReview = ''
+      if (this.inputReview != "") {
+        this.reviews.unshift(
+            {
+                review: this.inputReview,
+                rating: this.rating
+            });
+        this.inputReview = "";
+        this.rating=0
+      }
+    },
+    setRating(rating) {
+        this.rating = rating
     }
   },
 };
@@ -52,10 +67,10 @@ export default {
 
 <style scoped>
 li {
-    list-style: none;
+  list-style: none;
 }
 .tabContents {
-    text-align: left;
+  text-align: left;
 }
 
 .balloon5 {
@@ -70,7 +85,7 @@ li {
   width: 80px;
 }
 
-.balloon5 .faceicon img{
+.balloon5 .faceicon img {
   width: 100%;
   height: auto;
   border: solid 3px #d7ebfe;
@@ -83,7 +98,7 @@ li {
 
 .says {
   display: inline-block;
-  position: relative; 
+  position: relative;
   margin: 5px 0 0 105px;
   padding: 17px 13px;
   border-radius: 12px;
@@ -94,7 +109,7 @@ li {
   content: "";
   display: inline-block;
   position: absolute;
-  top: 18px; 
+  top: 18px;
   left: -24px;
   border: 12px solid transparent;
   border-right: 12px solid #d7ebfe;
