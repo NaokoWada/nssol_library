d1e491a9b0da08ee601149fa005ba746efa97d80
github NaokoWada <wada.naoko.7yq@jp.nssol.nipponsteel.com>
2021-12-10 17:56:47 +0900

    モーダルを追加

--- src/components/Book_list.vue
+++ src/components/Book_list.vue
@@ -1,261 +1,315 @@
 <template>
-
   <div id="app">
     <head>
-      <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
+      <link
+        href="https://fonts.googleapis.com/css?family=Noto+Sans+JP"
+        rel="stylesheet"
+      />
     </head>
     <div class="mainbody">
-     <tytle class="wf-notosansjapanese">
-       <font size="7" color="black">本の一覧</font>
-       <font size="4" color="black">
-         -現在登録されている本は 6 冊です-
-       </font></tytle>
+      <tytle class="wf-notosansjapanese">
+        <font size="7" color="black">本の一覧</font>
+        <font size="4" color="black">
+          -現在登録されている本は 6 冊です-
+        </font></tytle
+      >
     </div>
-    
-    <table border="1" style="border-collapse: collapse;">
-    <thead>
-    <tr>
-    <div keyword="app2" class="container">
-      <form>
-        <div class="form-group">
 
-          <!-- <small class="form-text">タイトルまたはキーワードを入力してください。</small> -->
-          <input type="text" v-model="keyword" class="form-control" id="id"
-           placeholder="タイトルまたはキーワードを入力してください。">
-          
-          <!-- <button type="submit" class="btn btn-primary">検索</button> -->
-              <!-- <div> -->
+    <table border="1" style="border-collapse: collapse">
+      <thead>
+        <tr>
+          <div keyword="app2" class="container">
+            <form>
+              <div class="form-group">
+                <!-- <small class="form-text">タイトルまたはキーワードを入力してください。</small> -->
+                <input
+                  type="text"
+                  v-model="keyword"
+                  class="form-control"
+                  id="id"
+                  placeholder="タイトルまたはキーワードを入力してください。"
+                />
 
-              <input class="visually-hidden" type="radio" id="option1" value="" v-model="level" />
-              <label for="option1">全て</label>
+                <!-- <button type="submit" class="btn btn-primary">検索</button> -->
+                <!-- <div> -->
 
-              <input class="visually-hidden" type="radio" id="option2" value="初級" v-model="level" />
-              <label for="option2">初級</label>
-              
-              <input class="visually-hidden" type="radio" id="option3" value="中級" v-model="level" />
-              <label for="option3">中級</label>
-              
-              <input class="visually-hidden" type="radio" id="option4" value="上級" v-model="level" />
-              <label for="option4">上級</label>
+                <input
+                  class="visually-hidden"
+                  type="radio"
+                  id="option1"
+                  value=""
+                  v-model="level"
+                />
+                <label for="option1">全て</label>
 
+                <input
+                  class="visually-hidden"
+                  type="radio"
+                  id="option2"
+                  value="初級"
+                  v-model="level"
+                />
+                <label for="option2">初級</label>
 
-              
+                <input
+                  class="visually-hidden"
+                  type="radio"
+                  id="option3"
+                  value="中級"
+                  v-model="level"
+                />
+                <label for="option3">中級</label>
 
-            <!-- <div class="form-group form-radio"> -->
-              <!-- <label><input type="radio" v-mode="checklevel" v-bind:value="book.level2">{{ book.level2}}</label> -->
-              <!-- <input type="radiobutton" class="form-radio-input" id="radio"> -->
-              <!-- <label class="form-radio-input" for="radio">radio button</label> -->
+                <input
+                  class="visually-hidden"
+                  type="radio"
+                  id="option4"
+                  value="上級"
+                  v-model="level"
+                />
+                <label for="option4">上級</label>
 
-        </div>
-      </form>
-    </div>
-    </tr>
-    </thead>
-    <thead style="display: block;">
-    <tr>
-    
-    <th style="width:150px;"></th><!-- 空欄用 -->
-    <th style="width:150px;">キーワード</th>
-    <th style="width:150px;">読みやすさ</th>
-    <th style="width:150px;">社内評価</th>
-    <th style="width:150px;">BU評価</th>
-    
-    <th style="width:440px;">読んだ人</th>
-    </tr>
-    </thead>
-    
-    <tbody style="display: block;overflow-y:scroll;height:600px;">
+                <!-- <div class="form-group form-radio"> -->
+                <!-- <label><input type="radio" v-mode="checklevel" v-bind:value="book.level2">{{ book.level2}}</label> -->
+                <!-- <input type="radiobutton" class="form-radio-input" id="radio"> -->
+                <!-- <label class="form-radio-input" for="radio">radio button</label> -->
+              </div>
+            </form>
+          </div>
+        </tr>
+      </thead>
+      <thead style="display: block">
+        <tr>
+          <th style="width: 150px"></th>
+          <!-- 空欄用 -->
+          <th style="width: 150px">キーワード</th>
+          <th style="width: 150px">読みやすさ</th>
+          <th style="width: 150px">社内評価</th>
+          <th style="width: 150px">BU評価</th>
 
-    <tr v-for="book in filteredbooks" v-bind:key="book.id">
-      <!--<div v-if="book.title== keyword || book.keyword==keyword">-->         
+          <th style="width: 440px">読んだ人</th>
+        </tr>
+      </thead>
 
-        <td style="width:150px;"><router-link :to="`/bookList/${book.id}`"><img :src="book.image">
-        <br>{{book.title}}</router-link>
-    
+      <tbody style="display: block; overflow-y: scroll; height: 600px">
+        <tr v-for="book in filteredbooks" v-bind:key="book.id">
+          <!--<div v-if="book.title== keyword || book.keyword==keyword">-->
 
-        
-        <td style="width:150px;">{{book.keyword}}</td>
-        <td style="width:150px;">{{book.level}}</td>
-        <td style="width:150px;">{{book.internal}}</td>
-        <td style="width:150px;">{{book.bu}}</td>
-    
-        <td style="width:440px;">
-          <ul class="example">
+          <td style="width: 150px">
+            <router-link :to="`/bookList/${book.id}`"
+              ><img :src="book.image" /> <br />{{ book.title }}</router-link
+            >
+          </td>
+
+          <td style="width: 150px">{{ book.keyword }}</td>
+          <td style="width: 150px">{{ book.level }}</td>
+          <td style="width: 150px">{{ book.internal }}</td>
+          <td style="width: 150px">{{ book.bu }}</td>
+
+          <td style="width: 440px">
+            <ul class="example">
               <li>
                 <img alt="user img" :src="book.employee1" />
                 <br />
-                <button class="buttonmenu" v-on:click="home">感想を見る</button>
+                <button class="buttonmenu" v-on:click="openModal">
+                  感想を見る
+                </button>
+                <MyModal @close="closeModal" v-if="modal">
+                  <p>{{ book.comment1 }}</p>
+                </MyModal>
               </li>
 
               <li>
                 <img alt="user img" :src="book.employee2" />
                 <br />
-                <button class="buttonmenu" v-on:click="home">感想を見る</button>
+                <button class="buttonmenu" v-on:click="openModal2">
+                  感想を見る
+                </button>
+                <MyModal @close="closeModal2" v-if="modal2">
+                  <p>{{ book.comment2 }}</p>
+                </MyModal>
               </li>
-          </ul>
-        </td>
-      <!--</div>--> 
-
-
-    </tr>
-  
-
-    
-    </tbody>
+            </ul>
+          </td>
+          <!--</div>-->
+        </tr>
+      </tbody>
     </table>
-    
   </div>
-
 </template>
 
 <script>
+import MyModal from "./MyModal.vue";
 export default {
+  components: { MyModal },
   data() {
     return {
-      keyword:"",
-      level:"",
-      books:[
-
-            {id: 1,
-              image:require('../assets/datamodel.png'),
-             title:'データモデル大全', 
-             internal:'★★★★☆',
-             bu:'★★★☆☆',
-             employee1:require('../assets/mask_woman5_faceshield.png'),
-             comment1:'データモデリングの実務上の具体例がわかりやすい！',
-             employee2:require('../assets/mask_woman6_medical.png'),
-             comment2:'もう少し具体例の難易度を高くすべき',
-             keyword:'データモデル',
-             level:'初級'},
-            {
-              id: 2,
-              image:require('../assets/NWhard.png'),
-             title:'ネットワーク', 
-             internal:'★★★★☆',
-             bu:'★★★☆☆',
-             employee1:require('../assets/mask_woman5_faceshield.png'),
-             comment1:'前提知識がある程度必要だと感じた。いい本',
-             employee2:require('../assets/mask_woman6_medical.png'),
-             comment2:'最新の高度なネットワーク技術について知ることができた',
-             keyword:'ネットワーク',
-             level:'上級'},
-             {id:3,
-             image:require('../assets/NWeasy.png'),
-             title:'ネットワーク構築＆運用がしっかりわかる教科書', 
-             internal:'★★★★★',
-             bu:'★★★★★',
-             employee1:require('../assets/mask_woman5_faceshield.png'),
-             comment1:'図が豊富に使われており分かりやすかった',
-             employee2:require('../assets/mask_woman6_medical.png'),
-             comment2:'初心者でもわかりやすいように説明だった',
-             keyword:'ネットワーク構築',
-             level:'初級'},
-            {
-              id: 4,
-             image:require('../assets/vendar.png'),
-             title:'ベンダーマネージメントの極意',
-             internal:'★★★★★',
-             bu:'★★★★☆',
-             employee1:require('../assets/mask_woman5_faceshield.png'),
-             comment1:'ためになりました！',
-             employee2:require('../assets/mask_woman6_medical.png'),
-             comment2:'そんなに読む価値なし！',
-             keyword:'マネジメント',
-             level:'中級'},
-             {
-               id: 5,
-             image:require('../assets/dx.jpg'),
-             title:'DXの思考法',
-             internal:'★★☆☆☆',
-             bu:'★★★☆☆',
-             employee1:require('../assets/mask_woman5_faceshield.png'),
-             comment1:'上級者向け',
-             employee2:require('../assets/mask_woman6_medical.png'),
-             comment2:'知りたいことが書いてなかった',
-             keyword:'DX',
-             level:'上級'},
-             {
-               id: 6,
-             image:require('../assets/NWospf.png'),
-             title:'OSPF Complete Implementation',
-             internal:'★★★★★',
-             bu:'★★★★★',
-             employee1:require('../assets/mask_woman5_faceshield.png'),
-             comment1:'OSPFについて詳しく知りたい人にはとてもオススメの本です！',
-             employee2:require('../assets/mask_woman6_medical.png'),
-             comment2:'OSPFについて完璧に理解できる。ただし内容は英語',
-             keyword:'OSPF',
-             level:'上級'}
-             ]
-          
-    }
+      modal: false,
+      modal2: false,
+      message: "",
+      keyword: "",
+      level: "",
+      books: [
+        {
+          id: 1,
+          image: require("../assets/datamodel.png"),
+          title: "データモデル大全",
+          internal: "★★★★☆",
+          bu: "★★★☆☆",
+          employee1: require("../assets/mask_woman5_faceshield.png"),
+          comment1: "データモデリングの実務上の具体例がわかりやすい！",
+          employee2: require("../assets/mask_woman6_medical.png"),
+          comment2: "もう少し具体例の難易度を高くすべき",
+          keyword: "データモデル",
+          level: "初級",
+        },
+        {
+          id: 2,
+          image: require("../assets/NWhard.png"),
+          title: "ネットワーク",
+          internal: "★★★★☆",
+          bu: "★★★☆☆",
+          employee1: require("../assets/mask_woman5_faceshield.png"),
+          comment1: "前提知識がある程度必要だと感じた。いい本",
+          employee2: require("../assets/mask_woman6_medical.png"),
+          comment2: "最新の高度なネットワーク技術について知ることができた",
+          keyword: "ネットワーク",
+          level: "上級",
+        },
+        {
+          id: 3,
+          image: require("../assets/NWeasy.png"),
+          title: "ネットワーク構築＆運用がしっかりわかる教科書",
+          internal: "★★★★★",
+          bu: "★★★★★",
+          employee1: require("../assets/mask_woman5_faceshield.png"),
+          comment1: "図が豊富に使われており分かりやすかった",
+          employee2: require("../assets/mask_woman6_medical.png"),
+          comment2: "初心者でもわかりやすいように説明だった",
+          keyword: "ネットワーク構築",
+          level: "初級",
+        },
+        {
+          id: 4,
+          image: require("../assets/vendar.png"),
+          title: "ベンダーマネージメントの極意",
+          internal: "★★★★★",
+          bu: "★★★★☆",
+          employee1: require("../assets/mask_woman5_faceshield.png"),
+          comment1: "ためになりました！",
+          employee2: require("../assets/mask_woman6_medical.png"),
+          comment2: "そんなに読む価値なし！",
+          keyword: "マネジメント",
+          level: "中級",
+        },
+        {
+          id: 5,
+          image: require("../assets/dx.jpg"),
+          title: "DXの思考法",
+          internal: "★★☆☆☆",
+          bu: "★★★☆☆",
+          employee1: require("../assets/mask_woman5_faceshield.png"),
+          comment1: "上級者向け",
+          employee2: require("../assets/mask_woman6_medical.png"),
+          comment2: "知りたいことが書いてなかった",
+          keyword: "DX",
+          level: "上級",
+        },
+        {
+          id: 6,
+          image: require("../assets/NWospf.png"),
+          title: "OSPF Complete Implementation",
+          internal: "★★★★★",
+          bu: "★★★★★",
+          employee1: require("../assets/mask_woman5_faceshield.png"),
+          comment1: "OSPFについて詳しく知りたい人にはとてもオススメの本です！",
+          employee2: require("../assets/mask_woman6_medical.png"),
+          comment2: "OSPFについて完璧に理解できる。ただし内容は英語",
+          keyword: "OSPF",
+          level: "上級",
+        },
+      ],
+    };
   },
 
   methods: {
+    openModal() {
+      this.modal = true;
+    },
+    openModal2() {
+      this.modal2 = true;
+    },
+    closeModal() {
+      this.modal = false;
+    },
+    closeModal2() {
+      this.modal2 = false;
+    },
+
     moveDetail(num) {
       this.$router.push({
         name: "Bookdetail",
         params: {
-          id: num
-        }
-      })
-    }
+          id: num,
+        },
+      });
+    },
   },
 
   computed: {
-      filteredbooks: function() {
-        var books = [];
-        for(var i in this.books) { 
-          var b  = this.books[i];  
-          if((b.keyword.indexOf(this.keyword) !== -1 || b.title.indexOf(this.keyword) !== -1) && b.level.indexOf(this.level) !== -1){ 
-            // || b.level.indexOf(this.level) !== -1
-            books.push(b); 
-          }
+    filteredbooks: function () {
+      var books = [];
+      for (var i in this.books) {
+        var b = this.books[i];
+        if (
+          (b.keyword.indexOf(this.keyword) !== -1 ||
+            b.title.indexOf(this.keyword) !== -1) &&
+          b.level.indexOf(this.level) !== -1
+        ) {
+          // || b.level.indexOf(this.level) !== -1
+          books.push(b);
         }
-        return books; 
       }
-    }
-
-
-}
-
+      return books;
+    },
+  },
+};
 </script>
 
 <style scoped>
-.wf-notosansjapanese { font-family: "Noto Sans JP"; }
+.wf-notosansjapanese {
+  font-family: "Noto Sans JP";
+}
 
 #app {
-
   text-align: center;
   margin-left: auto;
   margin-right: auto;
- 
 }
- .mainbody{
-   background-image: url(../assets/haikei.png);
-   text-align: center;
-   vertical-align: top;
- }
+.mainbody {
+  background-image: url(../assets/haikei.png);
+  text-align: center;
+  vertical-align: top;
+}
 
- .form-control{
-   width:25%;
-    margin-right: auto;
-    padding:15px 0;
-    margin-bottom:15px;
-    background-color:whitesmoke;
- }
+.form-control {
+  width: 25%;
+  margin-right: auto;
+  padding: 15px 0;
+  margin-bottom: 15px;
+  background-color: whitesmoke;
+}
 
- label {
+label {
   cursor: pointer;
   padding-left: 30px;
   position: relative;
 }
 
-
 label::before,
 label::after {
   content: "";
-  display: block; 
+  display: block;
   position: absolute;
 }
 
@@ -286,16 +340,16 @@ input:checked + label::after {
 }
 
 .visually-hidden {
- position: absolute;
- white-space: nowrap;
- border: 0;
- clip: rect(0 0 0 0);
- clip-path: inset(50%);
- overflow: hidden;
- height: 1px;
- width: 1px;
- margin: -1px;
- padding: 0;
+  position: absolute;
+  white-space: nowrap;
+  border: 0;
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  overflow: hidden;
+  height: 1px;
+  width: 1px;
+  margin: -1px;
+  padding: 0;
 }
 
 .example li {
@@ -304,5 +358,4 @@ input:checked + label::after {
   padding: 0;
   margin: 30px 50px;
 }
-
 </style>
